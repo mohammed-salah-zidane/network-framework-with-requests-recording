8ffb6c865cc3ebf3a2c07f3d6305a3b95e04ed75
Yousef Hamza <jo.adam.93@gmail.com>
2021-01-13 13:14:48 +0200

    Initial Commit

---
+++ InstabugInterview/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstabugInterview
+//
+//  Created by Yousef Hamza on 1/13/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
