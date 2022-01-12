a22da91656afaf59a20a5405e8bc59cbb8785bc4
Yousef Hamza <jo.adam.93@gmail.com>
2022-01-12 11:43:26 +0200

    Update task

--- InstabugNetworkClient/NetworkClient.swift
+++ InstabugNetworkClient/NetworkClient.swift
@@ -11,20 +11,34 @@ public class NetworkClient {
     public static var shared = NetworkClient()
 
     // MARK: Network requests
-    public func get(_ url: URL, payload: Any?=nil) {
-        fatalError("Not implemented")
+    public func get(_ url: URL, completionHandler: @escaping (Data?) -> Void) {
+        executeRequest(url, method: "GET", payload: nil, completionHandler: completionHandler)
     }
 
-    public func post(_ url: URL, payload: Any?=nil) {
-        fatalError("Not implemented")
+    public func post(_ url: URL, payload: Data?=nil, completionHandler: @escaping (Data?) -> Void) {
+        executeRequest(url, method: "POSt", payload: payload, completionHandler: completionHandler)
     }
 
-    public func put(_ url: URL, payload: Any?=nil) {
-        fatalError("Not implemented")
+    public func put(_ url: URL, payload: Data?=nil, completionHandler: @escaping (Data?) -> Void) {
+        executeRequest(url, method: "PUT", payload: payload, completionHandler: completionHandler)
     }
 
-    public func delete(_ url: URL, payload: Any?=nil) {
-        fatalError("Not implemented")
+    public func delete(_ url: URL, completionHandler: @escaping (Data?) -> Void) {
+        executeRequest(url, method: "DELETE", payload: nil, completionHandler: completionHandler)
+    }
+
+    func executeRequest(_ url: URL, method: String, payload: Data?, completionHandler: @escaping (Data?) -> Void) {
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = method
+        urlRequest.httpBody = payload
+        URLSession.shared.dataTask(with: urlRequest) { data, response, error in
+            #warning("Record request/response")
+            fatalError("Not implemented")
+        
+            DispatchQueue.main.async {
+                completionHandler(data)
+            }
+        }
     }
 
     // MARK: Network recording
